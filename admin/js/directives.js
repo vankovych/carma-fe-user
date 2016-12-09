453c9ee0f407f82161618c7cbe670ef5bf287136
ELEKS-SOFTWARE\oleksandr.chemerys <oleksandr.chemerys@eleks.com>
2016-12-09 12:42:39 +0200

    Added Requirements view

--- admin/js/directives.js
+++ admin/js/directives.js
@@ -1,7 +1,12 @@
-﻿
+﻿app.directive('requirementsDirective', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'requirementsView.html'
+    }
+});
 
 
-app.directive('positionView', function () {
+app.directive('positionView', function() {
     return {
         restrict: 'E',
         templateUrl: 'position-view.html'
---
+++ admin/js/reqScript.js
@@ -0,0 +1,16 @@
+﻿function myFunction() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+}
+
+function closeAddSpan() {  
+    var modal = document.getElementById('myModal');
+    modal.style.display ="none";
+    console.log("click");
+}
+window.onclick = function (event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
