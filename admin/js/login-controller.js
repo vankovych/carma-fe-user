50c19518367ee9db0d9a6b94bbf66f9499736adb
ELEKS-SOFTWARE\oleksandr.chemerys <oleksandr.chemerys@eleks.com>
2016-12-13 16:27:45 +0200

    no message

--- admin/js/login-controller.js
+++ admin/js/login-controller.js
@@ -1,17 +1,17 @@
 ﻿
 
 app.service('getTable', function () {
-    this.myFunc = function ($scope, $http) {     
+    this.myFunc = function ($scope, $http) {
         $http({
             url: 'http://localhost:3000/api/positions',
-            method: "GET",       
+            method: "GET",
             headers: {
-                'Authorization': 'Bearer uIP92SFNwZ06RyrQ',
+                'Authorization': 'Bearer GmSddICqaogEnWte',
                 'Content-Type': 'application/json'
             }
         })
     .success(function (response) {
-        
+
         console.log(response.data);
         $scope.dataTable = response.data;
         console.log(response.data);
@@ -33,10 +33,6 @@ app.service('getReq', function () {
         })
     .success(function (response) {
 
-<<<<<<< HEAD
-app.service('CustomPost', [ '$http' , function ($http) {
-    this.Communicate = function (reqType, url, dataBody) {
-=======
         console.log(response.data);
         $scope.reqTable = response.data;
         console.log(response.data);
@@ -46,15 +42,15 @@ app.service('CustomPost', [ '$http' , function ($http) {
     });
     };
 });
-app.service('CustomPost', function () {
-    this.Communicate = function ($scope, $http, reqType, url, dataBody) {
->>>>>>> 5879578288cf5d331d6091976c11929ea2a82ef4
+
+app.service('CustomPost', ['$http', function ($http) {
+    this.Communicate = function (reqType, url, dataBody) {
         $http({
             url: 'http://localhost:3000/api/' + url,
             method: reqType,
             data: dataBody,
             headers: {
-                'Authorization': 'Bearer uIP92SFNwZ06RyrQ',
+                'Authorization': 'Bearer GmSddICqaogEnWte',
                 'Content-Type': 'application/json'
             }
         })
@@ -69,35 +65,32 @@ app.service('CustomPost', function () {
     };
 }]);
 
-app.controller('loginController', ['$scope', '$http', 'getTable', 'CustomPost', 'getReq',function ($scope, $http, getTable, CustomPost, getReq) {
+app.controller('loginController', ['$scope', '$http', 'getTable', 'CustomPost', 'getReq', function ($scope, $http, getTable, CustomPost, getReq) {
 
     $scope.dataTable = getTable.myFunc($scope, $http);
-<<<<<<< HEAD
+
+    $scope.reqTable = getReq.myFunc($scope, $http);
     $scope.myPost = function (reqType, url, dataBody) {
         CustomPost.Communicate(reqType, url, dataBody);
     };
 
 
-=======
-    //$scope.Communication = CustomPost.Communicate($scope, $http, reqType, url, dataBody);
-    $scope.reqTable = getReq.myFunc($scope, $http);
->>>>>>> 5879578288cf5d331d6091976c11929ea2a82ef4
     $scope.submit = function () {
         var uname = $scope.username;
         var upassword = $scope.password;
-      
-    
+
+
         var config = {
             headers: {
                 'Content-Type': 'application/json'
             }
-        }
+        };
         var data = JSON.stringify({ login: uname, password: upassword });
 
         $http.post('http://localhost:3000/login', data, config)
         .success(function (data, status, headers, config) {
-            window.location.hash = '#/mainWindow';
             $scope.PostDataResponse = data;
+            window.location.hash = '#/mainWindow';
             console.log(data);
             console.log($scope.PostDataResponse);
         })
@@ -108,5 +101,5 @@ app.controller('loginController', ['$scope', '$http', 'getTable', 'CustomPost', 
             pass.className = "red";
         });
 
-    };      
+    };
 }]);
