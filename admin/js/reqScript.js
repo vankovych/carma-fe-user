875c16c457cc0267ae96ecbd73768ce5442c4c98
ELEKS-SOFTWARE\iryna.bokalo <iraklepak@gmail.com>
2016-12-15 16:39:49 +0200

    Add filter  for name and titles for positions

--- admin/js/reqScript.js
+++ admin/js/reqScript.js
@@ -1,11 +1,30 @@
-﻿function searchFunction() {
+﻿function changeOption()
+{
+    if (document.getElementById("select-block").value === "name") {
+        document.getElementById("select-block").setAttribute("data-option", "name");
+        console.log(document.getElementById("select-block").value);
+    }
+    else
+    {
+        document.getElementById("select-block").setAttribute("data-option", "title");
+        console.log(document.getElementById("select-block").value);
+    }
+}
+function searchFunction() {
+    if (document.getElementById("select-block").value == "title") {
+        var parameter = 1;
+    } else
+    {
+        var parameter = 0;
+    }
+    
     var input, filter, table, tr, td, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("posTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
+        td = tr[i].getElementsByTagName("td")[parameter];
         if (td) {
             if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
