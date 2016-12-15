39cdc75d3f93408cbaf50a1f66dd08f1feafe294
ELEKS-SOFTWARE\iryna.bokalo <iraklepak@gmail.com>
2016-12-15 15:55:13 +0200

    Add implementation of search bar by names. Change some design.

--- admin/js/reqScript.js
+++ admin/js/reqScript.js
@@ -1,4 +1,21 @@
-﻿    var lastModal;
+﻿function searchFunction() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("posTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+var lastModal;
 
     function myFunction(modalForm,sender,id) {
         lastModal = document.getElementById(modalForm);
